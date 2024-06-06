430bf0a5ebf27911e224f3c28f199c1b2f2bb70e
gichukipitar <ptahgichuki@yahoo.com>
2024-06-06 15:41:52 +0300

    Initial commit

---
+++ src/main/java/com/fineract/mifos/MifosApplication.java
@@ -0,0 +1,13 @@
+package com.fineract.mifos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MifosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MifosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fineract/mifos/MifosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fineract.mifos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MifosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
