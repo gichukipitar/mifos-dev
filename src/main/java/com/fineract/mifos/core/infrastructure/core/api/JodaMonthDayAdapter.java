b239b0ff7df6d7b1a463b73f55bc0a857534af0d
gichukipitar <ptahgichuki@yahoo.com>
2024-06-07 09:58:34 +0300

    serialization updates

--- src/main/java/com/fineract/mifos/core/infrastructure/core/api/JodaMonthDayAdapter.java
+++ src/main/java/com/fineract/mifos/core/infrastructure/core/api/JodaMonthDayAdapter.java
@@ -1,4 +1,27 @@
 package com.fineract.mifos.core.infrastructure.core.api;
 
-public class JodaMonthDayAdapter {
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.MonthDay;
+
+import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
+
+/**
+ * Serializer for Joda Time {@link MonthDay} that returns the date in array format to match previous Jackson
+ * functionality.
+ */
+public class JodaMonthDayAdapter implements JsonSerializer<MonthDay> {
+
+    @Override
+    @SuppressWarnings("unused")
+    public JsonElement serialize(final MonthDay src, final Type typeOfSrc, final JsonSerializationContext context) {
+        JsonArray array = null;
+        if (src != null) {
+            array = new JsonArray();
+            array.add(new JsonPrimitive(src.get(MONTH_OF_YEAR)));
+            array.add(new JsonPrimitive(src.getDayOfMonth()));
+        }
+        return array;
+    }
 }
