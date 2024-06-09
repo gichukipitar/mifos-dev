e95cd40ce559db0240bdb6799b3efba3d155a0b6
gichukipitar <ptahgichuki@yahoo.com>
2024-06-09 10:00:40 +0300

    AppUser repository

--- src/main/java/com/fineract/mifos/mifos_core/useradministration/repository/AppUserRepository.java
+++ src/main/java/com/fineract/mifos/mifos_core/useradministration/repository/AppUserRepository.java
@@ -1,7 +1,9 @@
 package com.fineract.mifos.mifos_core.useradministration.repository;
 
+import com.fineract.mifos.mifos_core.useradministration.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 }
