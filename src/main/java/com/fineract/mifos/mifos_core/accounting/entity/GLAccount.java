af2d0897b039f0137cd1e18a89104306a89d0869
gichukipitar <ptahgichuki@yahoo.com>
2024-06-11 11:59:12 +0300

    entity class

---
+++ src/main/java/com/fineract/mifos/mifos_core/accounting/entity/GLAccount.java
@@ -0,0 +1,56 @@
+package com.fineract.mifos.mifos_core.accounting.entity;
+
+import com.fineract.mifos.mifos_core.infrastructure.core.domain.AbstractPersistableCustom;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "acc_gl_account", uniqueConstraints = { @UniqueConstraint(columnNames = { "gl_code" }, name = "acc_gl_code") })
+@NoArgsConstructor
+@Accessors(chain = true)
+public class GLAccount extends AbstractPersistableCustom {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private GLAccount parent;
+
+    @Column(name = "hierarchy", nullable = true, length = 50)
+    private String hierarchy;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private List<GLAccount> children = new ArrayList<>();
+
+    @Column(name = "name", nullable = false, length = 45)
+    private String name;
+
+    @Column(name = "gl_code", nullable = false, length = 100)
+    private String glCode;
+
+    @Column(name = "disabled", nullable = false)
+    private boolean disabled;
+
+    @Column(name = "manual_journal_entries_allowed", nullable = false)
+    private boolean manualEntriesAllowed = true;
+
+    @Column(name = "classification_enum", nullable = false)
+    private Integer type;
+
+    @Column(name = "account_usage", nullable = false)
+    private Integer usage;
+
+    @Column(name = "description", nullable = true, length = 500)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private CodeValue tagId;
+
+
+}
