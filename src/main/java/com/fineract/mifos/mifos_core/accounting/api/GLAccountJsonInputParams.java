e7392b5c4d19f59875c902b1a96a554a4dcd0311
gichukipitar <ptahgichuki@yahoo.com>
2024-06-11 11:47:26 +0300

    GLA accountJsonInput parameters

---
+++ src/main/java/com/fineract/mifos/mifos_core/accounting/api/GLAccountJsonInputParams.java
@@ -0,0 +1,36 @@
+package com.fineract.mifos.mifos_core.accounting.api;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public enum GLAccountJsonInputParams {
+    ID("id"), NAME("name"), PARENT_ID("parentId"), GL_CODE("glCode"), DISABLED("disabled"), MANUAL_ENTRIES_ALLOWED(
+            "manualEntriesAllowed"), TYPE("type"), USAGE("usage"), DESCRIPTION("description"), TAGID("tagId");
+
+    private final String value;
+
+    GLAccountJsonInputParams(final String value) {
+        this.value = value;
+    }
+
+    private static final Set<String> values = new HashSet<>();
+
+    static {
+        for (final GLAccountJsonInputParams type : GLAccountJsonInputParams.values()) {
+            values.add(type.value);
+        }
+    }
+
+    public static Set<String> getAllValues() {
+        return values;
+    }
+
+    @Override
+    public String toString() {
+        return name().toString().replaceAll("_", " ");
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
