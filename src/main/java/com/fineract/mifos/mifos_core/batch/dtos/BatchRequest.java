a6a45f55f878d35aed0d0666656c7dc8223920fc
gichukipitar <ptahgichuki@yahoo.com>
2024-06-12 11:39:52 +0300

    batch request

---
+++ src/main/java/com/fineract/mifos/mifos_core/batch/dtos/BatchRequest.java
@@ -0,0 +1,30 @@
+package com.fineract.mifos.mifos_core.batch.dtos;
+
+import io.swagger.v3.oas.annotations.headers.Header;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.Set;
+
+/**
+ * Provides an object for separate HTTP requests in the Batch Request for Batch API. A requestId is also included as
+ * data field which takes care of dependency issues among various requests. This class also provides getter and setter
+ * functions to access Batch Request data fields.
+ *
+ * @author Peter Gichuki
+ *
+ *
+ * @see Header
+ */
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class BatchRequest {
+    private Long requestId;
+    private String relativeUrl;
+    private String method;
+    private Set<Header> headers;
+    private Long reference;
+    private String body;
+}
