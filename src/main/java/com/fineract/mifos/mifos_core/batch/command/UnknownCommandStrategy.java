536574c3ebfc1675ebe531e7cd315489c290d5f0
gichukipitar <ptahgichuki@yahoo.com>
2024-06-12 11:17:04 +0300

    unknown command strategy

---
+++ src/main/java/com/fineract/mifos/mifos_core/batch/command/UnknownCommandStrategy.java
@@ -0,0 +1,19 @@
+package com.fineract.mifos.mifos_core.batch.command;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Provides a default CommandStrategy by implementing {@link org.apache.fineract.batch.command.CommandStrategy} in case
+ * there is no appropriate command strategy with requested 'method' and 'resoureUrl'.
+ *
+ * @author Rishabh Shukla
+ */
+@Component
+public class UnknownCommandStrategy implements CommandStrategy {
+    @Override
+    public BatchResponse execute(BatchRequest batchRequest, @SuppressWarnings("unused") UriInfo uriInfo) {
+        return new BatchResponse().setRequestId(batchRequest.getRequestId()).setStatusCode(SC_NOT_IMPLEMENTED)
+                .setBody("Resource with method " + batchRequest.getMethod() + " and relativeUrl " + batchRequest.getRelativeUrl()
+                        + " doesn't exist");
+    }
+}
