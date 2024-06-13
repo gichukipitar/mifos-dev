124081013a7cd94026abe35cc356d19be8f0b961
gichukipitar <ptahgichuki@yahoo.com>
2024-06-13 09:45:44 +0300

    Batch filter class updates

--- src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/filters/BatchFilter.java
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/filters/BatchFilter.java
@@ -1,4 +1,11 @@
 package com.fineract.mifos.mifos_core.infrastructure.core.filters;
 
+import com.fineract.mifos.mifos_core.batch.dtos.BatchRequest;
+import com.fineract.mifos.mifos_core.batch.dtos.BatchResponse;
+
+import javax.ws.rs.core.UriInfo;
+
 public interface BatchFilter {
+    BatchResponse doFilter(BatchRequest batchRequest, UriInfo uriInfo, BatchFilterChain chain);
+
 }
