9ae808c67cdde0bc7d50907265bf3b96f186de15
gichukipitar <ptahgichuki@yahoo.com>
2024-06-17 08:42:11 +0300

    Business date update handler

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/businessdate/handler/BusinessDateUpdateHandler.java
@@ -0,0 +1,23 @@
+package com.fineract.mifos.mifos_core.infrastructure.businessdate.handler;
+
+import com.fineract.mifos.mifos_core.commands.annotation.CommandType;
+import com.fineract.mifos.mifos_core.commands.handler.NewCommandSourceHandler;
+import com.fineract.mifos.mifos_core.infrastructure.core.api.JsonCommand;
+import com.fineract.mifos.mifos_core.infrastructure.core.data.CommandProcessingResult;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@CommandType(entity = "BUSINESS_DATE", action = "UPDATE")
+public class BusinessDateUpdateHandler implements NewCommandSourceHandler {
+    private final BusinessDateWritePlatformService businessDateWritePlatformService;
+
+    @Transactional
+    @Override
+    public CommandProcessingResult processCommand(@NotNull final JsonCommand command) {
+        return businessDateWritePlatformService.updateBusinessDate(command);
+    }
+}
