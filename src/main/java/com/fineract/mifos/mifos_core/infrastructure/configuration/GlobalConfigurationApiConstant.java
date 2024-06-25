990e02195154022e4c2bab911c0ce2157eeafd07
gichukipitar <ptahgichuki@yahoo.com>
2024-06-25 09:45:39 +0300

    global config constants

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/configuration/GlobalConfigurationApiConstant.java
@@ -0,0 +1,18 @@
+package com.fineract.mifos.mifos_core.infrastructure.configuration;
+
+public class GlobalConfigurationApiConstant {
+
+    private GlobalConfigurationApiConstant() {
+
+    }
+
+    public static final String ENABLED = "enabled";
+    public static final String VALUE = "value";
+    public static final String DATE_VALUE = "dateValue";
+    public static final String ID = "id";
+    public static final String CONFIGURATION_RESOURCE_NAME = "globalConfiguration";
+    public static final String localeParamName = "locale";
+    public static final String dateFormatParamName = "dateFormat";
+    public static final String STRING_VALUE = "stringValue";
+
+}
