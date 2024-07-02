27364eb4b55738c85dd9b36b5270e31128c3dbc1
gichukipitar <ptahgichuki@yahoo.com>
2024-07-02 11:31:16 +0300

    fineract profiles

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/boot/FineractProfiles.java
@@ -0,0 +1,10 @@
+package com.fineract.mifos.mifos_core.infrastructure.core.boot;
+
+public class FineractProfiles {
+    public static final String LIQUIBASE_ONLY = "liquibase-only";
+    public static final String DIAGNOSTICS = "diagnostics";
+
+    public static final String TEST = "test";
+
+    private FineractProfiles() {}
+}
