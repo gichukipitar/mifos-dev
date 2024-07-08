6c4532314f5c0e509c5829fae391b6984d9d084e
gichukipitar <ptahgichuki@yahoo.com>
2024-07-08 11:03:20 +0300

    platform limit validation exception

--- src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exception/PlatformInternalServerException.java
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exception/PlatformInternalServerException.java
@@ -1,5 +1,8 @@
 package com.fineract.mifos.mifos_core.infrastructure.core.exception;
 
+/**
+ * A {@link RuntimeException} thrown when unexpected server side errors happen.
+ */
 public class PlatformInternalServerException extends AbstractPlatformException {
 
     public PlatformInternalServerException(String globalisationMessageCode, String defaultUserMessage, Object... defaultUserMessageArgs) {
---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exception/PlatformRequestBodyItemLimitValidationException.java
@@ -0,0 +1,9 @@
+package com.fineract.mifos.mifos_core.infrastructure.core.exception;
+
+public class PlatformRequestBodyItemLimitValidationException extends RuntimeException{
+
+    public PlatformRequestBodyItemLimitValidationException(String message) {
+        super(message);
+    }
+
+}
