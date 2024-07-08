b40a662ab0cf3cfc378478268a99c3bdb6b67f5b
gichukipitar <ptahgichuki@yahoo.com>
2024-07-08 09:43:08 +0300

    abstract platform service

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exception/AbstractPlatformServiceUnavailableException.java
@@ -0,0 +1,12 @@
+package com.fineract.mifos.mifos_core.infrastructure.core.exception;
+
+/**
+ * A {@link RuntimeException} thrown for cases such as 3rd-party external services that are not available.
+ */
+public abstract class AbstractPlatformServiceUnavailableException extends AbstractPlatformException {
+
+    protected AbstractPlatformServiceUnavailableException(String globalisationMessageCode, String defaultUserMessage,
+                                                          Object... defaultUserMessageArgs) {
+        super(globalisationMessageCode, defaultUserMessage, defaultUserMessageArgs);
+    }
+}
