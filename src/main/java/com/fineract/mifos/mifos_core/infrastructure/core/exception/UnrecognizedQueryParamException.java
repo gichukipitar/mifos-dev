2ea6d07e190b3d500323c5a2bf8e4baa321c122a
gichukipitar <ptahgichuki@yahoo.com>
2024-07-08 11:07:03 +0300

    unrecognized query parameter exception

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exception/UnrecognizedQueryParamException.java
@@ -0,0 +1,18 @@
+package com.fineract.mifos.mifos_core.infrastructure.core.exception;
+
+import lombok.Getter;
+
+@Getter
+public class UnrecognizedQueryParamException extends RuntimeException {
+
+    private final String queryParamKey;
+    private final String queryParamValue;
+    private final Object[] supportedParams;
+
+    public UnrecognizedQueryParamException(final String queryParamKey, final String queryParamValue, final Object... supportedParams) {
+        this.queryParamKey = queryParamKey;
+        this.queryParamValue = queryParamValue;
+        this.supportedParams = supportedParams;
+    }
+
+}
--- src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exception/UnsupportedParameterException.java
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exception/UnsupportedParameterException.java
@@ -1,14 +1,18 @@
 package com.fineract.mifos.mifos_core.infrastructure.core.exception;
 
+import lombok.Getter;
+
 import java.util.List;
 
+/**
+ * A {@link RuntimeException} that is thrown in the case where invalid parameters are sent in the body of the request to
+ * the platform api.
+ */
+@Getter
 public class UnsupportedParameterException extends RuntimeException{
     private final List<String> unsupportedParameters;
 
     public UnsupportedParameterException(final List<String> unsupportedParameters) {
         this.unsupportedParameters = unsupportedParameters;
-    }
-    public List<String> getUnsupportedParameters() {
-        return this.unsupportedParameters;
     }
 }
