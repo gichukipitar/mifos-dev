045e5e186f3ed6c72484328c3e34ba4a6e58835f
gichukipitar <ptahgichuki@yahoo.com>
2024-07-10 09:19:09 +0300

    invalid tenant identifier exception

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/exceptionmapper/InvalidTenantIdentifierExceptionMapper.java
@@ -0,0 +1,32 @@
+package com.fineract.mifos.mifos_core.infrastructure.core.exceptionmapper;
+
+import com.fineract.mifos.mifos_core.infrastructure.core.data.ApiGlobalErrorResponse;
+import com.fineract.mifos.mifos_core.infrastructure.core.exception.ErrorHandler;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * An {@link ExceptionMapper} to map {@link InvalidTenantIdentifierException} thrown by platform during authentication
+ * into a HTTP API friendly format.
+ *
+ * The {@link InvalidTenantIdentifierException} is thrown by spring security on platform when a request contains an
+ * invalid tenant identifier.
+ */
+@Provider
+@Component
+@Scope("singleton")
+@Slf4j
+public class InvalidTenantIdentifierExceptionMapper implements ExceptionMapper<InvalidTenantIdentifierException> {
+
+    @Override
+    public Response toResponse(@SuppressWarnings("unused") final InvalidTenantIdentifierException exception) {
+        log.warn("Exception occurred", ErrorHandler.findMostSpecificException(exception));
+        return Response.status(Response.Status.UNAUTHORIZED).entity(ApiGlobalErrorResponse.invalidTenantIdentifier())
+                .type(MediaType.APPLICATION_JSON).build();
+    }
+}
