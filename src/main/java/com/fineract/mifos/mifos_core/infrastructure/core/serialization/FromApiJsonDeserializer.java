b955340ff95c96fac4d0f49ba84bb7aa1facf423
gichukipitar <ptahgichuki@yahoo.com>
2024-07-13 13:08:03 +0300

    json helper

--- src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/serialization/FromApiJsonDeserializer.java
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/serialization/FromApiJsonDeserializer.java
@@ -1,4 +1,6 @@
 package com.fineract.mifos.mifos_core.infrastructure.core.serialization;
 
-public interface FromApiJsonDeserializer {
+public interface FromApiJsonDeserializer<T> {
+
+    T commandFromApiJson(String json);
 }
