71f64724650e65908c980af7fd8d778a17bc5f09
gichukipitar <ptahgichuki@yahoo.com>
2024-07-14 12:16:16 +0300

    IndexDetail

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/core/service/database/IndexDetail.java
@@ -0,0 +1,11 @@
+package com.fineract.mifos.mifos_core.infrastructure.core.service.database;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class IndexDetail {
+
+    private final String indexName;
+}
