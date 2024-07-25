36b44ef5b35c484b00cbc1899c9ff77921b629f4
gichukipitar <ptahgichuki@yahoo.com>
2024-07-25 09:15:30 +0300

    MessageId

---
+++ src/main/java/com/fineract/mifos/mifos_core/infrastructure/event/external/service/messages/dto/MessageId.java
@@ -0,0 +1,11 @@
+package com.fineract.mifos.mifos_core.infrastructure.event.external.service.messages.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class MessageId {
+
+    private final int id;
+}
